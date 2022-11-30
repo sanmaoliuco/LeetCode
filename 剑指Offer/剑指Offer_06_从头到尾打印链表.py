962c50601ed92a9486004b36be1a7bbdee26458d
sanmaoliuco <2236471892@qq.com>
2022-11-30 15:27:45 +0800

    update

---
+++ 剑指Offer/剑指Offer_06_从头到尾打印链表.py
@@ -0,0 +1,58 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time : 2022/11/30 14:54
+# @Author : San_mao_liu
+# @File : 剑指Offer_06_从头到尾打印链表.py
+# @Software: PyCharm
+
+
+# 定义结点
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class SingleLinkList(object):
+    def __init__(self):
+        self.head = None
+
+if __name__ == '__main__':
+    link_list = SingleLinkList()
+    node1 = ListNode(1)
+    node2 = ListNode(2)
+    node3 = ListNode(3)
+
+    link_list.head = node1
+    node1.next = node2
+    node2.next = node3
+
+    # print(link_list.head.val)
+    # print(link_list.head.next.val)
+    s = []
+    while link_list.head:
+        s.append(link_list.head.val)
+        link_list.head = link_list.head.next
+    print(s)
+
+    # 递归
+    def reversePrint(head):
+
+        return reversePrint(head.next) + [head.val] if head else []
+
+    print(reversePrint(node1))
+
+    # 辅助栈
+    def reverse_print(head):
+        s = []
+        while head:
+            s.append(head.val)
+            head = head.next
+
+        return s[::-1]
+
+    print(reverse_print(node1))
+
+
+
+
+
